d7d85caf26a1dda7cb61ad23ea1c660e6dc43ac2
regaw-leinad <selmersaxman@gmail.com>
2015-06-02 15:36:06 -0700

    Fix compile

--- FinalProject/FaceBoxFrameHandler.cpp
+++ FinalProject/FaceBoxFrameHandler.cpp
@@ -8,15 +8,6 @@ void FaceBoxFrameHandler::handleFaceDetectionFrame(Mat &frame, const int &keyPre
 		Face face = faces[i];
 
 		rectangle(frame, face.getFace(), Scalar(0, 255, 0), 4);
-
-		for (size_t j = 0; j < faces[i].getEyes().size(); j++) {
-			Rect eye = faces[i].getEyes()[j];
-
-			Point center(face.getFace().x + eye.x + eye.width * 0.5, face.getFace().y + eye.y + eye.height * 0.5);
-			int radius = cvRound((eye.width + eye.height) * 0.25);
-
-			circle(frame, center, radius, Scalar(255, 0, 0), 4, 8, 0);
-		}
 	}
 }
 
