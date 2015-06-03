0f184ae5dffed95dd02807c55832db3262ce3a28
regaw-leinad <selmersaxman@gmail.com>
2015-06-02 20:50:18 -0700

    Remove old haar cascades and update props for 2.4.11

--- FinalProject/main.cpp
+++ FinalProject/main.cpp
@@ -3,13 +3,9 @@
 const int CAMERA_WIDTH = 800;
 const int CAMERA_HEIGHT = 600;
 
-//void onMouseEvent(int event, int x, int y, int flags, void* userdata) {
-//}
-
 int main(int argc, char *argv[]) {
 	PhotoBooth487 photoBooth(CAMERA_WIDTH, CAMERA_HEIGHT);
 	photoBooth.start();
 
 	return 0;
 }
-
